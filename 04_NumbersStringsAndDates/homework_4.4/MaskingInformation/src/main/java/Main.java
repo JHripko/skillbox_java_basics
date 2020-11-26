6111eb55af6df8fb68d3a45db748185d427258c4
Evgeny <ev.hripko@gmail.com>
2020-11-26 10:06:12 +0300

    Masking information 4.4 homework *

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -2,10 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String text = "Номер банковской карты <0000> 0000 <0000> 0000";
+        String safe = searchAndReplaceDiamonds(text, "****");
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+        for (int i = 0; i <= text.length(); i++) {
+            if (text.indexOf('<') >=0 && text.indexOf('>') >= 0) {
+                int beginDiamond = text.indexOf('<');
+                int endDiamond = text.indexOf('>');
+
+                //выбираем номер который в кавычках
+                String hideNumber = text.substring(beginDiamond, endDiamond + 1);
+                //меняем содержимое в кавычках на placeholder
+                text = text.replaceAll(hideNumber, placeholder);
+            }
+        }
         return text;
     }
 
