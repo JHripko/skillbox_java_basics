de937b1936940b777c59923ffcebf097102645f3
Евгений <ev.hripko@gmail.com>
2020-12-07 18:51:07 +0300

    Full name formatter 4.5 homework

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,17 +2,58 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            } else {
+                fullNameFormatter(input);
+            }
+        }
     }
-  }
 
+    //дополнительные методы
+    //метод преобразования введенной строки в формат записи ФИО
+    public static void fullNameFormatter(String input) {
+
+        if (isCorrect(input)) {
+            String[] arrayText = input.split(" ");
+
+            System.out.println("Фамилия: " + arrayText[0]);
+            System.out.println("Имя: " + arrayText[1]);
+            System.out.println("Отчество: " + arrayText[2]);
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
+    }
+
+    //метод определения содержит ли строка цифры
+    public static boolean isNumber(String input) {
+        String text = input
+                .replaceAll("\\s+", "")
+                .trim();
+
+        if (text.matches("\\d+")) {
+            return false;
+        } else return true;
+    }
+
+    //метод проверки содержания трех слов
+    public static boolean isThreeWords(String input) {
+        String[] arrayText = input.split(" ");
+
+        if (arrayText.length == 3) {
+            return true;
+        } else return false;
+    }
+
+    //метод проверки корректности записи
+    public static boolean isCorrect(String input) {
+        if (isNumber(input) && isThreeWords(input)) {
+            return true;
+        } else return false;
+    }
 }
