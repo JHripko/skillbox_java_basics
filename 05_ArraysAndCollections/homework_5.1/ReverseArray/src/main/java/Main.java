a6d47663b8c4c743694a5223c1ef922b1ff21831
Evgeny <ev.hripko@gmail.com>
2020-12-14 13:43:57 +0300

    Reverse Array 5.1 homework

--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+
         String line = "Каждый охотник желает знать, где сидит фазан";
+        //создаем массив из слов строки
+        String[] strings = line.split(",?\\s+");
+
+        //создаем массив, равный "перевернутому" массиву strings
+        String[] reverseStrings = ReverseArray.reverse(strings);
+
+        //преобразуем в строку
+        String arrText = Arrays.toString(reverseStrings);
+
+        System.out.print(arrText);
     }
+
 }
