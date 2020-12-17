7db63a8625dc023ffd4a2873eccdbde0a1acfb91
Evgeny <ev.hripko@gmail.com>
2020-12-17 13:56:29 +0300

    Email List 5.3 homework

--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,37 @@
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
+    //создаем список
+    static TreeSet<String> myEmailList = new TreeSet<>();
+    //маска email адреса
+    private static final String EMAIL_PATTERN =
+                                "^[_A-Za-z0-9]+(\\.[_A-Za-z0-9-]+)*@" +
+                                "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
     public void add(String email) {
-        // TODO: валидный формат email добавляется
+        email = email.toLowerCase();
+        if (isEmailValid(email)) {
+            myEmailList.add(email);
+            System.out.println("Email успешно добавлен!");
+        } else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new LinkedList<>(myEmailList);
+    }
+
+    //дополнительные методы
+    //валидатор email
+    public static boolean isEmailValid(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
 }
--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
@@ -1,21 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    static String command = "";         //переменная команды (LIST, ADD)
+    static String email = "";           //переменная email адреса
+
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
+    public static final String WRONG_COMMAND_ANSWER = "Команда не распознана!";
+
+    private static EmailList emailList = new EmailList();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,9 +20,65 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+
+            inputParser(input);
+
+            resetAll();
         }
+    }
+
+
+    //дополнительные методы
+    //разделение команды
+    public static void inputParser(String input) {
+        input = input.trim();
+
+        //создаем массив элементов из строки
+        String[] commands = input.split("\\s");
+
+        //первый элемент по порядку должен быть командой
+        command = commands[0];
+
+        //проверяем на наличие текста после команды
+        if (commands.length == 2) {
+            email = commands[1];
+            commandDefinition(command);
+        } else if (commands.length == 1) {
+            commandDefinition(command);
+        } else {
+            System.out.println(WRONG_EMAIL_ANSWER);
+        }
+    }
+
+    //определение команды и исполнение
+    public static void commandDefinition(String command) {
+        switch (command) {
+            case "ADD":
+                emailList.add(email);
+                break;
+
+            case "LIST":
+                getList();
+                break;
+
+            default:
+                System.out.println(WRONG_COMMAND_ANSWER);
+                break;
+        }
+    }
+
+    //отображение списка
+    public static void getList() {
+        List<String> myList = new LinkedList<>(emailList.getSortedEmails());
+
+        for (String emailAddress : myList) {
+            System.out.println(emailAddress);
+        }
+    }
+
+    //сброс значений
+    public static void resetAll() {
+        command = "";
+        email = "";
     }
 }
