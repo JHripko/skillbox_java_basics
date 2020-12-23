4b6f6f23225e48286dfabca6f94d1ae35381fd0d
Evgeny <ev.hripko@gmail.com>
2020-12-23 16:13:13 +0300

    Cool Numbers 5.5 homework

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -2,26 +2,73 @@ import java.util.*;
 
 public class CoolNumbers {
 
+    //создаем HashSet список с номерами
+    public static HashSet<String> gosNumbersAsHashSet = new HashSet<>();
+    public static TreeSet<String> gosNumbersAsTreeSet = new TreeSet<>();
+
     public static List<String> generateCoolNumbers() {
 
-        return Collections.emptyList();
+        String[] letters = {"А","В","Е","К","М","О","Н","С","Т","Р","У","Х"};
+
+        Arrays.sort(letters);
+
+        for (int number = 111; number <= 999; number += 111) {
+            for (String a : letters) {
+                for (String b : letters) {
+                    for (String c : letters) {
+                        for (int region = 1; region <= 199; region++) {
+                            gosNumbersAsHashSet.add(String.format("%s%03d%s%s%d", a, number, b, c, region));
+                        }
+                    }
+                }
+            }
+        }
+        ArrayList<String> gosNumbersAsList = new ArrayList<>(gosNumbersAsHashSet);
+        gosNumbersAsTreeSet.addAll(gosNumbersAsHashSet);
+
+        return gosNumbersAsList;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        boolean isFound = false;
+
+        for (String gosNumber : list) {
+            if (gosNumber.equals(number)) {
+                isFound = true;
+                break;
+            }
+        }
+        return isFound;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        int index = Collections.binarySearch(sortedList, number);
+
+        return (index >=0);
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        boolean isFound = false;
+        for (String entry : hashSet) {
+            if (entry.equals(number)) {
+                isFound = true;
+                break;
+            }
+        }
+
+        return isFound;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        boolean isFound = false;
+        for (String entry : treeSet) {
+            if (entry.equals(number)) {
+                isFound = true;
+                break;
+            }
+        }
+        return isFound;
     }
 
 }
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,13 +1,96 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+
+    public static CoolNumbers coolNumbers = new CoolNumbers();
+
+    public static ArrayList<String> carNumbersAsList = (ArrayList<String>) CoolNumbers.generateCoolNumbers();
+    public static HashSet<String> carNumbersAsHashSet = CoolNumbers.gosNumbersAsHashSet;
+    public static TreeSet<String> carNumberAsTreeSet = CoolNumbers.gosNumbersAsTreeSet;
+
+    public static String number = "Х555АК167";
+
+    public static long bruteForceTime;
+    public static long binarySearchTime;
+    public static long hashSetSearchTime;
+    public static long treeSetSearchTime;
 
     public static void main(String[] args) {
 
+        getBruteForceSearch();
+        //сортируем список
+        Collections.sort(carNumbersAsList);
+
+        getBinarySearch();
+        getHashSetSearch();
+        getTreeSetSearch();
+
+        //выводим самый быстрый результат в консоль
+        getCompare();
+
+    }
+
+    //дополнительные методы
+    //поиск перебором
+    public static void getBruteForceSearch() {
+        long start = System.nanoTime();
+        if (CoolNumbers.bruteForceSearchInList(carNumbersAsList, number)) {
+            long end = System.nanoTime();
+            bruteForceTime = end - start;
+            System.out.println("Поиск перебором. Номер: " + number + " Время: " + bruteForceTime + " нс");
+        } else {
+            System.out.println("Не найдено");
+        }
+    }
+
+    //бинарный поиск
+    public static void getBinarySearch() {
+        long start = System.nanoTime();
+        if (CoolNumbers.binarySearchInList(carNumbersAsList, number)) {
+            long end = System.nanoTime();
+            binarySearchTime = end - start;
+            System.out.println("Бинарный поиск. Номер: " + number + " Время: " + binarySearchTime + " нс");
+        } else {
+            System.out.println("Не найдено");
+        }
+    }
+
+    //поиск в HashSet
+    public static void getHashSetSearch() {
+        long start = System.nanoTime();
+        if (CoolNumbers.searchInHashSet(carNumbersAsHashSet, number)) {
+            long end = System.nanoTime();
+            hashSetSearchTime = end - start;
+            System.out.println("Поиск в HashSet. Номер: " + number + " Время: " + hashSetSearchTime + " нс");
+        } else {
+            System.out.println("Не найдено");
+        }
+    }
+
+    //поиск в TreeSet
+    public static void getTreeSetSearch() {
+        long start = System.nanoTime();
+        if (CoolNumbers.searchInTreeSet(carNumberAsTreeSet, number)) {
+            long end = System.nanoTime();
+            treeSetSearchTime = end - start;
+            System.out.println("Поиск в TreeSet. Номер: " + number + " Время: " + treeSetSearchTime + " нс");
+        } else {
+            System.out.println("Не найдено");
+        }
+    }
+
+    //сравнение
+    public static void getCompare() {
+        long first = Math.min(bruteForceTime, binarySearchTime);
+        long second = Math.min(hashSetSearchTime, treeSetSearchTime);
+
+        if (first > second) {
+            System.out.println("Самый быстрый результат: " + second + " нс");
+        } else if (first < second) {
+            System.out.println("Самый быстрый результат: " + first + " нс");
+        }
     }
 }
