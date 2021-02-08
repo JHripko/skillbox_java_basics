539a6cca769d886e02df5107ea68fa91a3b6c4d3
Evgeny <ev.hripko@gmail.com>
2021-02-08 15:54:43 +0300

    Homework 7.2 - Airport

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,31 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    public static Date currentDate = new Date(System.currentTimeMillis());          //переменная текущего времени
+    public static Date afterTime = new Date(System.currentTimeMillis() + 7200000);  //переменная времени через 2 ч
     public static void main(String[] args) {
 
-
+        Airport airport = Airport.getInstance();
+        findPlanesLeavingInTheNextTwoHours(airport).forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return airport.getTerminals()
+                .stream()
+                .flatMap(terminal -> terminal.getFlights().stream()
+                        .filter((flight -> flight.getDate()
+                                //фильтруем рейсы из списка по дате через 2 ч от текущей даты
+                                .after(currentDate) & flight.getDate().before(afterTime)
+                                    //также фильтруем по статусу рейса "вылетает"
+                                    & (flight.getType() == Flight.Type.DEPARTURE))))
+                //собираем в список
+                .collect(Collectors.toList());
     }
 
 }
