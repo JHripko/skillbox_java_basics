7de8fa9a46e4019ca9adb83e23b2e3b2560af0cc
Evgeny <ev.hripko@gmail.com>
2021-02-16 13:51:42 +0300

    Homework 9.1 - File Utils

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long length = 0;                            //размер файла
+        File folder = new File(path);               //инициализируем переменную типа File
+        File[] files = folder.listFiles();          //создаем массив из файлов в текущей дериктории
+
+        try {
+            for (File file : files) {
+                //проверяем, если текущий элемент массива является файлом то вычисляем его размер
+                if (file.isFile()) {
+                    length += file.length();
+                    //если текущий элемент массива является папкой то рекурсивно вызываем метод и передаем в него
+                    //путь к текущей папки и вычисляем размеры файлов в ней
+                } else {
+                    length += calculateFolderSize(file.getPath());
+                }
+            }
+        }
+        catch (Exception exception) {
+            System.out.println(exception);
+            System.out.println(Arrays.toString(exception.getStackTrace()));
+        }
+
+        return length;
+    }
+
+    //дополнительный метод
+    //метод форматирования размера директории
+    public static String formatSize(long size) {
+        String unit = "байт";
+        if (size > 1024) {
+            size = size / 1024;
+            unit = "Кб";
+        }
+
+        if (size > 1024) {
+            size = size / 1024;
+            unit = "Мб";
+        }
+
+        if (size > 1024) {
+            size = size / 1024;
+            unit = "Гб";
+        }
+
+        return " " + size + " " + unit;
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static boolean program = true;
+
+    public static void main(String[] args) throws Exception {
+        while (program) {
+            System.out.println("Введите путь до папки:");
+            Scanner in = new Scanner(System.in);
+            String path = in.nextLine().trim();
+
+            long size = FileUtils.calculateFolderSize(path);
+            System.out.println(FileUtils.formatSize(size));
+        }
     }
 }
