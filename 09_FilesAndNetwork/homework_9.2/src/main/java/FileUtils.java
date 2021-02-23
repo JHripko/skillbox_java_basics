a4bfe2521765787d8bafb9383a0019fedaf5d080
Evgeny <rus16hripko@gmail.com>
2021-02-23 12:28:47 +0300

    Homework 9.2 - File Utils (copy directory)

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,5 +1,47 @@
+import java.io.*;
+
 public class FileUtils {
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File srcFile = new File(sourceDirectory);
+        File destFile = new File(destinationDirectory);
+
+        //если папка
+        if (srcFile.isDirectory()) {
+            //создаем директорию
+            if (!destFile.exists()) {
+                destFile.mkdir();
+            }
+
+            String[] children = srcFile.list();
+            for (int i = 0; i < children.length; i++) {
+                File srcFolder = new File(srcFile, children[i]);
+                File destFolder = new File(destFile, children[i]);
+
+                //рекурсивно вызываем метод для перехода к следующм папкам
+                copyFolder(srcFolder.getPath(), destFolder.getPath());
+            }
+        } else {
+            //если файл
+            try {
+                //создаем потоки
+                InputStream inputStream = new FileInputStream(srcFile);
+                OutputStream outputStream = new FileOutputStream(destFile);
+
+                //копируем побитово файл
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+
+                inputStream.close();
+                outputStream.close();
+            }
+            catch (IOException exception) {
+                exception.getStackTrace();
+                System.out.println("Операция не выполнена");
+            }
+        }
     }
 }
--- 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
@@ -1,4 +1,66 @@
+import java.util.Scanner;
+
 public class Main {
+    private static boolean program = true;
+    private static final String PATH_FORMAT_ERROR = "Неверный формат пути";
+    private static final String COMMAND_ERROR = "Команда не распознана";
+
     public static void main(String[] args) {
+        while (program) {
+            System.out.println("Введите команду (help для справки, exit для выхода):");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine().trim();
+
+            getCommand(input);
+        }
+    }
+
+
+    //дополнительные методы
+    //определение команды
+    public static void getCommand(String input) {
+        String[] data = input.split("\\s+");
+
+        if (data.length != 0) {
+            switch (data[0]) {
+                case "cp":
+                    getPaths(data);
+                    break;
+
+                case "help":
+                    getHelp();
+                    break;
+
+                case "exit":
+                    program = false;
+                    break;
+
+                default:
+                    System.out.println(COMMAND_ERROR);
+                    break;
+            }
+        }
+    }
+
+    //передаем пути папок
+    public static void getPaths(String[] data) {
+        if (isContainsPaths(data)) {
+            String sourceDirectory = data[1];
+            String destinationDirectory = data[2];
+            FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+        } else {
+            System.out.println(PATH_FORMAT_ERROR);
+        }
+    }
+
+    //проверяем наличие пути в команде
+    public static boolean isContainsPaths(String[] data) {
+        return data.length == 3 && data[1] != null && data[2] != null;
+    }
+
+    //отображение справки
+    public static void getHelp() {
+        System.out.println("Формат команды:");
+        System.out.println("cp SOURCE_PATH DESTINATION_PATH");
     }
 }
